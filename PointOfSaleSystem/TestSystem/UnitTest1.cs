f4c9d0d7f309ba30dc050dd2f0699c4e9740c706
EskilNTI <eskil.tornberg@elev.ga.ntig.se>
2024-11-07 11:00:32 +0100

    Added a test for ther total price of a product

--- PointOfSaleSystem/TestSystem/UnitTest1.cs
+++ PointOfSaleSystem/TestSystem/UnitTest1.cs
@@ -22,35 +22,38 @@ namespace TestSystem
                 var testCustomerOrderListBox = window.FindFirstDescendant(cf.ByAutomationId("customerOrderListBox"))?.AsListBox();
                 var testTotalPriceTextBlock = window.FindFirstDescendant(cf.ByAutomationId("TotalPriceTextBlock"))?.AsLabel();
 
-                if (testPlus1Espresso == null)
-                {
-                    Trace.WriteLine("Button 'plus1Espresso' not found.");
-                    Assert.Fail("Button 'plus1Espresso' not found.");
-                }
-
-                if (testCustomerOrderListBox == null)
-                {
-                    Trace.WriteLine("ListBox 'customerOrderListBox' not found.");
-                    Assert.Fail("ListBox 'customerOrderListBox' not found.");
-                }
-
-                if (testTotalPriceTextBlock == null)
-                {
-                    Trace.WriteLine("TextBlock 'TotalPriceTextBlock' not found.");
-                    Assert.Fail("TextBlock 'TotalPriceTextBlock' not found.");
-                }
-
                 testPlus1Espresso.Click();
                 testPlus1Espresso.Click();
                 testPlus1Espresso.Click();
                 Assert.AreEqual("3 | Espresso", testCustomerOrderListBox.Items[0].Text);
 
                 var resetButton = window.FindFirstDescendant(cf.ByAutomationId("ResetButton"))?.AsButton();
-                if (resetButton == null)
-                {
-                    Trace.WriteLine("Button 'ResetButton' not found.");
-                    Assert.Fail("Button 'ResetButton' not found.");
-                }
+
+                resetButton.Click();
+                Assert.AreEqual(0, testCustomerOrderListBox.Items.Length);
+                Assert.AreEqual("Total Price: 0 SEK", testTotalPriceTextBlock.Text);
+            }
+        }
+    
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var app = FlaUI.Core.Application.Launch(@"..\..\..\..\PointOfSaleSystem\bin\Debug\net8.0-windows\PointOfSaleSystem.exe");
+            using (var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+                ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+                Trace.WriteLine(window.Title);
+
+                var testPlus1Espresso = window.FindFirstDescendant(cf.ByAutomationId("plus1Espresso"))?.AsButton();
+                var testCustomerOrderListBox = window.FindFirstDescendant(cf.ByAutomationId("customerOrderListBox"))?.AsListBox();
+                var testTotalPriceTextBlock = window.FindFirstDescendant(cf.ByAutomationId("TotalPriceTextBlock"))?.AsLabel();
+
+                testPlus1Espresso.Click();
+                Assert.AreEqual("Total Price: 32 SEK", testTotalPriceTextBlock.Text);
+
+                var resetButton = window.FindFirstDescendant(cf.ByAutomationId("ResetButton"))?.AsButton();
 
                 resetButton.Click();
                 Assert.AreEqual(0, testCustomerOrderListBox.Items.Length);
