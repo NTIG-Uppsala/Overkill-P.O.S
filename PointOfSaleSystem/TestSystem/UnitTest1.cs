c42b8d67c2040d09c8f64b69ded010dc6fbd2fbc
EskilNTI <eskil.tornberg@elev.ga.ntig.se>
2024-11-11 09:37:57 +0100

    new Tests for More products

--- PointOfSaleSystem/TestSystem/UnitTest1.cs
+++ PointOfSaleSystem/TestSystem/UnitTest1.cs
@@ -60,5 +60,60 @@ namespace TestSystem
                 Assert.AreEqual("Total Price: 0 SEK", testTotalPriceTextBlock.Text);
             }
         }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var app = FlaUI.Core.Application.Launch(@"..\..\..\..\PointOfSaleSystem\bin\Debug\net8.0-windows\PointOfSaleSystem.exe");
+            using (var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+                ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+                Trace.WriteLine(window.Title);
+
+                var testPlus1Tea = window.FindFirstDescendant(cf.ByAutomationId("plus1Tea"))?.AsButton();
+                var testCustomerOrderListBox = window.FindFirstDescendant(cf.ByAutomationId("customerOrderListBox"))?.AsListBox();
+                var testTotalPriceTextBlock = window.FindFirstDescendant(cf.ByAutomationId("TotalPriceTextBlock"))?.AsLabel();
+
+                testPlus1Tea.Click();
+                testPlus1Tea.Click();
+                testPlus1Tea.Click();
+                Assert.AreEqual("3 | Tea", testCustomerOrderListBox.Items[0].Text);
+                Assert.AreEqual("Total Price: 75 SEK", testTotalPriceTextBlock.Text);
+
+                var resetButton = window.FindFirstDescendant(cf.ByAutomationId("ResetButton"))?.AsButton();
+
+                resetButton.Click();
+                Assert.AreEqual(0, testCustomerOrderListBox.Items.Length);
+                Assert.AreEqual("Total Price: 0 SEK", testTotalPriceTextBlock.Text);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var app = FlaUI.Core.Application.Launch(@"..\..\..\..\PointOfSaleSystem\bin\Debug\net8.0-windows\PointOfSaleSystem.exe");
+            using (var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+                ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+                Trace.WriteLine(window.Title);
+
+                var testPlus1Mocha = window.FindFirstDescendant(cf.ByAutomationId("plus1Mocha"))?.AsButton();
+                var testCustomerOrderListBox = window.FindFirstDescendant(cf.ByAutomationId("customerOrderListBox"))?.AsListBox();
+                var testTotalPriceTextBlock = window.FindFirstDescendant(cf.ByAutomationId("TotalPriceTextBlock"))?.AsLabel();
+
+                testPlus1Mocha.Click();
+                testPlus1Mocha.Click();
+                Assert.AreEqual("2 | Mocha", testCustomerOrderListBox.Items[0].Text);
+                Assert.AreEqual("Total Price: 70 SEK", testTotalPriceTextBlock.Text);
+
+                var resetButton = window.FindFirstDescendant(cf.ByAutomationId("ResetButton"))?.AsButton();
+
+                resetButton.Click();
+                Assert.AreEqual(0, testCustomerOrderListBox.Items.Length);
+                Assert.AreEqual("Total Price: 0 SEK", testTotalPriceTextBlock.Text);
+            }
+        }
     }
 }
