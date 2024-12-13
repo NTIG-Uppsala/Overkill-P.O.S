20312f92e9b5e97d3371c4f36e0df715a6cf8c92
NTI-Tim <tim.kelso@elev.ga.ntig.se>
2024-12-13 15:55:37 +0100

    Migration to Vite & Tailwind

---
+++ vite-pos-system/eslint.config.js
@@ -0,0 +1,23 @@
+// import eslintPluginPrettier from 'eslint-plugin-prettier';
+// import eslintPluginTailwindcss from 'eslint-plugin-tailwindcss';
+
+// export default [
+//   {
+//     files: ['**/*.{js,ts,jsx,tsx}'],
+//     plugins: {
+//       prettier: eslintPluginPrettier,
+//       tailwindcss: eslintPluginTailwindcss,
+//     },
+//     languageOptions: {
+//       ecmaVersion: 'latest',
+//       sourceType: 'module',
+//     },
+//     rules: {
+//       'prettier/prettier': 'error',
+//     },
+//   },
+// ];
+
+// ESLINT DOESN'T YET SUPPORT TAILWINDCSS V4.0 BETA. WHEN IT DOES, YOU COULD USE THE ABOVE CONFIG.
+// IN THE MEANTIME:
+// "npm run lint" will throw an error.
